5e05009639e36b3642c4ef14b117c5d4be84e244
D0ctrine <49787852+D0ctrine@users.noreply.github.com>
2019-06-14 09:19:52 +0900

    Add files via upload

---
+++ javaBasic0614/src/Basicstatic/example.java
@@ -0,0 +1,11 @@
+package Basicstatic;
+
+public class example {
+
+	
+	static String   name = "hi";
+	
+public static void say() {
+	System.out.println(name);
+}
+}
---
+++ javaBasic0614/src/Basicstatic/main.java
@@ -0,0 +1,19 @@
+package Basicstatic;
+
+import Basicstatic.example;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+			// TODO Auto-generated constructor stub
+			example good = new example();
+			System.out.println(good.name);
+			
+			System.out.println(example.name);
+			example.say();
+		
+		
+	}
+	
+}
