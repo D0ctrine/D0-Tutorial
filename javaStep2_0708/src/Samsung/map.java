e0dd78d3e6f90534bda98696b32dcc57ae7c5e54
D0ctrine <49787852+D0ctrine@users.noreply.github.com>
2019-07-09 22:48:05 +0900

    Add files via upload

--- javaStep2_0708/src/Samsung/map.java
+++ javaStep2_0708/src/Samsung/map.java
@@ -1,5 +1,6 @@
 package Samsung;
 
+import java.awt.List;
 import java.util.Random;
 
 public class map {
@@ -9,23 +10,33 @@ public class map {
 	void mapping(int a,int b){
 		String[][] map = new String[a][b];
 		Random r = new Random();
-		for(int i=0;i<a;i++) {
-			
-			for(int j=0;j<b;j++) {
-				if((i%(a-1))==0 || j%(b-1)==0) { //행 맨 위와 맨 밑/열 맨 앞과 맨 뒤
+		int x=0,y=0;
+		for(int i=0;i<a ;i++) {
+			for(int j=0;j<b ;j++) {
+				if((i%(a-1))==0 || j%(b-1)==0) { //행 맨 위와 맨 밑 / 열 맨 앞과 맨 뒤
 					map[i][j]="#";
-				}else if(r.nextInt(2)==1) {
-					map[i][j]=".";		//최소 3개이상 
-					
+				}else if(r.nextInt(3)==1) {
+					map[i][j]="#";
 				}else {
-					map[i][j]="#";
+					map[i][j]=".";
 				}
-				
-				
-				
-				
 			}
 		}
+		x=r.nextInt(a-2)+1;
+		y=r.nextInt(b-2)+1;
+		map[x][y]="R";
+		do {
+			x=r.nextInt(a-2)+1;
+			y=r.nextInt(b-2)+1;
+		}while(!map[x][y].equals("#"));
+			map[x][y]="B";
+		
+			do {
+				x=r.nextInt(a-2)+1;
+				y=r.nextInt(b-2)+1;
+			}while(!map[x][y].equals("#"));
+				map[x][y]="O";
+		
 		
 		//출력부
 		for(int i=0;i<a;i++) {
---
+++ javaStep2_0708/src/Samsung/move.java
@@ -0,0 +1,10 @@
+package Samsung;
+
+public class move {
+
+	
+	public void run(String[][] a) {
+		
+	}
+	
+}
